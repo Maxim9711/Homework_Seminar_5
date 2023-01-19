0ec08483b09a4866f5cbedfc916f98c3686cc533
Max <Bruce.97@mail.ru>
2023-01-19 17:42:06 +0300

    task 34

---
+++ задача_34/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] randomArray (int length){
+
+    int [] array = new int [length];
+
+    for (int i=0; i < array.Length; i++){
+        array[i] = new Random().Next(100, 1000);
+    }
+
+    return array;
+}
+
+int evenSymbol(int[] array) {
+    int countSymbol = 0;
+
+    for (int i=0; i < array.Length; i++)
+        if (array[i]%2 == 0)
+            countSymbol++;
+
+    return countSymbol;
+}
+
+void showArray (int[] array){
+    for(int i=0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.Write("\n");
+}
+
+// Формируем массив и выводим количество четных чисел в массиве  
+int [] array = randomArray(5);
+showArray(array);
+Console.WriteLine(evenSymbol(array));
